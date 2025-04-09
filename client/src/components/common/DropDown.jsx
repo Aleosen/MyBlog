e2cc798bfa4e58d6406bbe6ec7774e382fc6e03b
Aleosen <dongohago@gmail.com>
2025-04-09 18:21:10 +0300

    9.04.2025

--- client/src/components/common/DropDown.jsx
+++ client/src/components/common/DropDown.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function DropDown({ title, children }) {
   return (
     <div className="relative inline-block group">
-      <button className="px-4 py-2 hover:bg-gray-100 transition-colors flex items-center">
+      <button className="px-4 py-2 transition-colors flex items-center">
         {title}
       </button>
       <div className="z-20 absolute flex flex-col right-0 top-full pt-3 w-48 bg-white shadow-xl
